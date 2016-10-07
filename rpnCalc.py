4c9d57694e200c461b84891664d864f52be61374
TBartl <tbartl@umich.edu>
2016-10-07 14:24:54 -0400

    Added calc with ability to add

---
+++ rpnCalc.py
@@ -0,0 +1,19 @@
+def calculate (arg):
+    stack = list()
+    for token in arg.split():
+        if (token == "+"):
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+
+    return stack.pop()
+
+def main():
+    while True:
+        calculate(input("rpn calc> "))
+
+if __name__ == '__main__':
+    main()
