d9fb8d0e6f147d62b25cb821455433d235e6085e
TBartl <tbartl@umich.edu>
2016-10-07 14:25:21 -0400

    Added subtraction test case

--- test.py
+++ test.py
@@ -5,3 +5,7 @@ class TestBasics(unittest, TestCase):
     def test_add(self):
         results = rpn.calculate("1 1 +"):
         self.assertEqual(2, result)
+
+    def test_subtract(self):
+        results = rpn.calculate("5 3 -"):
+        self.assertEqual(2, result)
