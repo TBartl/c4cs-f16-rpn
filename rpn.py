b7c4a5a55eb34fbfae6e78cba41cc6733aa5f918
TBartl <tbartl@umich.edu>
2016-10-07 14:36:21 -0400

    Added  multiplication and division functionality

--- rpn.py
+++ rpn.py
@@ -10,6 +10,16 @@ def calculate (arg):
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = arg1 - arg2
+            stack.append(result)            
+        elif (token == "*"):
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 * arg2
+            stack.append(result)            
+        elif (token == "/"):
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 / arg2
             stack.append(result)
         else:
             stack.append(int(token))
