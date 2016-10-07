7f2878a9beed72bff80359ba9ac38f475caaa700
TBartl <tbartl@umich.edu>
2016-10-07 14:35:26 -0400

    Added division and multiplication test cases

--- test_rpn.py
+++ test_rpn.py
@@ -10,6 +10,15 @@ class TestBasics(unittest, TestCase):
         results = rpn.calculate("5 3 -"):
         self.assertEqual(2, result)
 
-    def test_toomanythings():
+    def test_toomanythings(self):
         with self.assertRaises(TypeError):
             rpn.calculate("1 1 1 +")
+
+    def test_multiply(self):
+        results = rpn.calculate("3 4 *")
+        self.assertEqual(12, results)
+
+    def test_divide(self):
+        results = rpn.calculate("12 4 /")
+        self.assertEqual(3, results)
+            
