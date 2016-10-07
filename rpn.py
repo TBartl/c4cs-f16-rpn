9e5edc597a9552421de6bc9f3018c8edc69a6601
TBartl <tbartl@umich.edu>
2016-10-07 14:27:27 -0400

    Renamed calculator

---
+++ rpn.py
@@ -0,0 +1,24 @@
+def calculate (arg):
+    stack = list()
+    for token in arg.split():
+        if (token == "+"):
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        elif (token == "-"):
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 - arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+
+    return stack.pop()
+
+def main():
+    while True:
+        calculate(input("rpn calc> "))
+
+if __name__ == '__main__':
+    main()
